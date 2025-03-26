37e15900ab9756d8e76a3dc7ec51b62b3335af4f
Daniyel Orr <daniyel.orr@gmail.com>
2025-03-26 11:58:25 +0200

    restore Tora API

--- bible-core/src/test/kotlin/dnl/bible/json/ToraApiTest.kt
+++ bible-core/src/test/kotlin/dnl/bible/json/ToraApiTest.kt
@@ -1,30 +1,22 @@
 package dnl.bible.json
 
-import dnl.bible.api.*
+import dnl.bible.api.ParashaEnum
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import java.io.File
 
 class ToraApiTest {
-//    companion object {
-//        lateinit var tora: Tora
-//        @BeforeAll @JvmStatic fun setup() {
-//            val bible = BibleLoader.loadFrom(File("../../json-bible-hebrew/bible-json-files/bible-just_letters-1.0.zip"))
-//            tora = ToraImpl(bible)
-//        }
-//    }
-//
-//    @Test
-//    fun testParasha() {
-//        var parasha = tora.getParasha(ParashaEnum.BALAK)
-//        var verses = parasha.getVerses()
-//        assertEquals(104, verses.size)
-//
-//        parasha = tora.getParasha(ParashaEnum.BEREISHIS)
-//        verses = parasha.getVerses()
-//        assertEquals(146, verses.size)
-//
-//    }
+    val tora = ToraImpl(bible)
+
+    @Test
+    fun testParasha() {
+        var parasha = tora.getParasha(ParashaEnum.BALAK)
+        var verses = parasha.getVerses()
+        assertEquals(104, verses.size)
+
+        parasha = tora.getParasha(ParashaEnum.BEREISHIS)
+        verses = parasha.getVerses()
+        assertEquals(146, verses.size)
+
+    }
 
 }
